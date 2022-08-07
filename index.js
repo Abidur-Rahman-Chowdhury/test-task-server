6e05b62e69156dfc8424b6bef2b1c68a4948f0a3
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-08-07 17:49:38 +0600

    create post end point to store data on database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { config } = require('dotenv');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,28 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Test server is running');
 })
+
+// making connection with MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@furniture-inventory-man.tnveo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const dataCollection = client.db("inventory").collection("dataCollection");
+
+        // post data on database for store data
+        app.post('/storeData', async (req, res) => {
+            const data = req.body;
+            const result = await dataCollection.insertOne(data);
+            res.send(result);
+        } )
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`listening port ${port}`);
